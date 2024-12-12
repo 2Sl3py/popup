950013455feb2701860a8d5194bbda2fa84e3276
Bentavious <bentavious@TheHiddProject6.hsd1.ga.comcast.net>
2024-12-12 16:38:50 -0500

    first commit

---
+++ static/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded"), () =>({
+     const modal = document.getElementById("myModal")
+   const openModalButton = document.getElementById("openmodal")
+    const closeModalButton = document.getElementById("closemodal")
+
+openModalButton.addEventListener
+
+    open.addEventListener("click"), () =>{
+         modal.style.display = 'block';
+
+})
+
+close.addEventListener("click"), () =>{
+    modal.style.display = 'none';
+})
